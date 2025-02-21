8d52507e874b35d954cd0c955868a2647335007d
Faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-02-21 17:46:53 +0700

    making a camera function using expo-camera

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./components/homeScreen";
+import Camera from "./components/camera";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <HomeScreen />
       <StatusBar style="light" />
+      {/* <Camera /> */}
     </View>
   );
 }
