d236eef6334f882458e5c5bd720af259a043de4c
Faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-02-21 11:41:32 +0700

    making homeScreen

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./components/homeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
+      <StatusBar style="light" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  texto: {
+    color: "white",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/homeScreen.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+export default function HomeScreen() {
+  return (
+    <>
+      <View style={style.textInputBox}>
+        <TextInput placeholder="Masukan NIM" maxLength={12} />
+      </View>
+      <View style={style.textInputBox}>
+        <TextInput placeholder="Masukan Token" maxLength={12} />
+      </View>
+    </>
+  );
+}
+
+const style = StyleSheet.create({
+  textInputBox: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 8,
+    width: 150,
+    margin: 10,
+  },
+  texto: {
+    color: "white",
+  },
+});
